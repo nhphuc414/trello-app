2e84a529c41038a2165f008cce060fb54a3e4481
Nguyen Hong Phuc <nhphuc414@gmail.com>
2024-10-18 15:22:05 +0700

    connect to mongodb and init environment variables

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,37 @@
-const express = require('express')
+/* eslint-disable no-console */
+import { env } from '~/config/environment'
+import express from 'express'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import exitHook from 'async-exit-hook'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const host = 'localhost'
-const port = '8080'
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to my API Server</h1>')
-})
+  app.get('/', (req, res) => {
+    res.send('<h1>Welcome to my API Server</h1>')
+  })
 
-app.listen(port, host, () => {
-  // eslint-disable-next-line no-console
-  console.log(`This server is running in http://${host}:${port}`)
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(
+      `3. Hi ${env.AUTHOR},This server is running in http://${env.APP_HOST}:${env.APP_PORT}`
+    )
+  })
+
+  exitHook(() => {
+    console.log('4.Disconnecting from MongoDB Cloud Atlas!')
+    CLOSE_DB()
+    console.log('5.Disconnected from MongoDB Cloud Atlas!')
+  })
+}
+
+;(async () => {
+  try {
+    console.log('1. Connection to MongoDB Cloud Atlas!')
+    CONNECT_DB()
+    console.log('2. Connected to MongoDB Cloud Atlas!')
+    START_SERVER()
+  } catch (error) {
+    console.log(error)
+    process.exit(0)
+  }
+})()
