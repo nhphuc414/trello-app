9cb3296becbb66bd6ce3f61043b1f563c8fd7048
Nguyen Hong Phuc <nhphuc414@gmail.com>
2024-10-19 09:23:03 +0700

    create Error handling

---
+++ server/src/middlewares/errorHandlingMiddleware.js
@@ -0,0 +1,14 @@
+import { StatusCodes } from 'http-status-codes'
+
+export const errorHandlingMiddleware = (err, req, res, next) => {
+  if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+
+  const responseError = {
+    statusCode: err.statusCode,
+    message: err.message || StatusCodes[err.statusCode],
+    stack: err.stack
+  }
+  // console.log(responseError)
+  // if (env.BUILD_MODE !== 'dev') delete responseError.stack
+  res.status(responseError.statusCode).json(responseError)
+}
--- server/src/server.js
+++ server/src/server.js
@@ -4,14 +4,12 @@ import express from 'express'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import exitHook from 'async-exit-hook'
 import { APIs_V1 } from './routes/v1'
+import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
 const START_SERVER = () => {
   const app = express()
   app.use(express.json())
   app.use('/v1', APIs_V1)
-  app.get('/', (req, res) => {
-    res.send('<h1>Welcome to my API Server</h1>')
-  })
-
+  app.use(errorHandlingMiddleware)
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(
       `3. Hi ${env.AUTHOR},This server is running in http://${env.APP_HOST}:${env.APP_PORT}`
