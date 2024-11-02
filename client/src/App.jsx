cf2d640a4789e320a00bdebdd3aeef69b3c2fec9
Nguyen Hong Phuc <nhphuc414@gmail.com>
2024-11-02 10:30:52 +0700

    add mui

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Button from '@mui/material/Button'
+import AccessAlarmsIcon from '@mui/icons-material/AccessAlarms'
+function App() {
+  return (
+    <>
+      <div>Hello</div>
+      <Button variant='text'>Text</Button>
+      <Button variant='contained'>Contained</Button>
+      <Button variant='outlined'>Outlined</Button>
+      <AccessAlarmsIcon />
+    </>
+  )
+}
+
+export default App
