be15fb8d459e2c6ad94f14ee1ad68a64650392c1
Nguyen Hong Phuc <nhphuc414@gmail.com>
2024-11-02 11:13:27 +0700

    config theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
+import theme from './theme.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 )
