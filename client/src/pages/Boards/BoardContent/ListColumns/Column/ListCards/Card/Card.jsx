9ac2e206d7fd31a3ca3c7cc41a689c1817c9f717
Nguyen Hong Phuc <nhphuc414@gmail.com>
2024-11-05 15:40:14 +0700

    split into components, rename some components

---
+++ client/src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -0,0 +1,42 @@
+import Typography from '@mui/material/Typography'
+import { Card as MuiCard } from '@mui/material'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Button from '@mui/material/Button'
+import CommentIcon from '@mui/icons-material/Comment'
+import AttachmentIcon from '@mui/icons-material/Attachment'
+import GroupIcon from '@mui/icons-material/Group'
+function Card() {
+  return (
+    <MuiCard
+      sx={{
+        cursor: 'pointer',
+        boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+        overflow: 'unset'
+      }}
+    >
+      <CardMedia
+        sx={{ height: 140 }}
+        image='https://img.pokemondb.net/artwork/large/charizard.jpg'
+        title='green iguana'
+      />
+      <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+        <Typography>Mern Stack</Typography>
+      </CardContent>
+      <CardActions sx={{ p: '0 4px 8px 4px' }}>
+        <Button size='small' startIcon={<GroupIcon />}>
+          20
+        </Button>
+        <Button size='small' startIcon={<CommentIcon />}>
+          15
+        </Button>
+        <Button size='small' startIcon={<AttachmentIcon />}>
+          10
+        </Button>
+      </CardActions>
+    </MuiCard>
+  )
+}
+
+export default Card
