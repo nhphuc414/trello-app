f5c2e4a6497df95350e438cd08e376bdbd0a9f8b
Nguyen Hong Phuc <nhphuc414@gmail.com>
2024-11-08 12:02:43 +0700

    feat: add redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,12 +6,15 @@ import CssBaseline from '@mui/material/CssBaseline'
 import theme from './theme.js'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+
+import { Provider } from 'react-redux'
+import { store } from '~/redux/store'
 createRoot(document.getElementById('root')).render(
-  //<StrictMode>
-  <CssVarsProvider theme={theme}>
-    <CssBaseline />
-    <App />
-    <ToastContainer position='bottom-left' theme='colored' autoClose={2000} />
-  </CssVarsProvider>
-  //</StrictMode>
+  <Provider store={store}>
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <App />
+      <ToastContainer position='bottom-left' theme='colored' autoClose={2000} />
+    </CssVarsProvider>
+  </Provider>
 )
